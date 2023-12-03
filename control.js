54c38f114857e3e040f2577445bf3b706efec1a9
kelaaj <120742273+kelaaj@users.noreply.github.com>
2023-12-03 03:08:28 -0500

    Update control.js

--- control.js
+++ control.js
@@ -1,43 +1,53 @@
 $(document).ready(function() {
     // Popup functionality
     $("#MyDscroll").hide();
-   // $("#mysubmit").hide();
-
-    // Call openForm when the document is ready
-    setTimeout(openForm, 5000);
-    function openForm(){
-
-    };
-
+  
     function openForm() {
-        // Use jQuery to toggle the visibility of the form
-        $("#MyDscroll").show();
+      // Use jQuery to toggle the visibility of the form
+      $("#MyDscroll").show();
     }
-
-    //php message
-    //function gojo() {
-      //  $("#MyDscroll").hide();
-        //$("#mysubmit").show();
-    //}
-
+  
+    // Call openForm after 5000 milliseconds (5 seconds)
+    var timerID = setTimeout(openForm, 5000);
+  
+    // Event handler for form submission
+    $("#signupbabe").on("submit", function(event) {
+      // Prevent the default form submission
+      event.preventDefault();
+  
+      // Get the value of the first name input
+      var firstName = $('#fname').val();
+  
+      // Hide the form
+      $("#MyDscroll").hide();
+  
+      // Replace the content of the formoutput element with the welcome message
+      $("#formoutput").html(" Welcome: " + firstName);
+  
+      // Clear the previously set timeout
+      clearTimeout(timerID);
+    });
+    
     // Function for redirection using location.replace
     function myReplace() {
-        var elOutput = document.getElementById("butt1");
-        location.replace(elOutput.href);
+      var elOutput = document.getElementById("butt1");
+      location.replace(elOutput.href);
     }
-
+  
     // Assign the function myReplace to the onclick property of the element with id "topoff1"
     document.getElementById("topoff1").onclick = function() {
-        myReplace();
+      myReplace();
     };
-
+  
     // Function for redirection to a specific URL
     function redirectw3() {
-        window.location.href = "https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_redirect_webpage";
+      window.location.href = "https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_redirect_webpage";
     }
-
+  
     // Assign the function redirectw3 to the onclick property of the element with id "butt1"
     document.getElementById("butt1").onclick = function() {
-        redirectw3();
+      redirectw3();
     };
-});
+  });
+  
+  
